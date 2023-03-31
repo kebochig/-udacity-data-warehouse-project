2e844c3c98cae3eae19869e215ebfd1e4bb3118f
kebochig <44658604+kebochig@users.noreply.github.com>
2023-03-31 19:54:31 +0100

    Update create_tables.py

--- create_tables.py
+++ create_tables.py
@@ -4,18 +4,42 @@ from sql_queries import create_table_queries, drop_table_queries
 
 
 def drop_tables(cur, conn):
+    """
+        This drops all the tables if they exist by calling the drop_table_queries defined in sql_queries.py.
+        Parameters
+        ----------
+        cur: class
+            This executes the sql commands within a database session.
+        conn: class
+            This takes care of the connection to a database instance.
+    """
     for query in drop_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def create_tables(cur, conn):
+     """
+        This creates tables if they don't exist by calling the create_table_queries defined in sql_queries.py.
+        Parameters
+        ----------
+        cur: class
+            This executes the sql commands within a database session.
+        conn: class
+            This takes care of the connection to a database instance.
+    """
     for query in create_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def main():
+    """
+        This executes the following:
+            1.) Call the create cluster script.
+            2.) Establish a db connection.
+            3.) Execute the drop and create table methods.
+    """
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
